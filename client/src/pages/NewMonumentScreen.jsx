a0a836af39ffb5b12722aeb50a0d7a99d090e9de
alexpapadopoulos <apapadopoulos@deloitte.gr>
2025-02-24 01:59:33 +0200

    refactor newmonumentscreen

--- client/src/pages/NewMonumentScreen.jsx
+++ client/src/pages/NewMonumentScreen.jsx
@@ -1,52 +1,82 @@
-import { useState, useCallback } from 'react'
-import { Link } from 'react-router-dom'
+import { useState, useCallback, useEffect } from 'react'
 import axios from 'axios'
 import GenericMap from '../components/GenericMap'
 import Pin from '../components/Pin'
 import {Marker} from 'react-map-gl'
 import { GeolocateControl, NavigationControl } from 'react-map-gl'
-import { INIT_MAP_STATE, MAPBOX_ACCESS_TOKEN } from '../utils/constants'
-import useAppStore from '../utils/AppStore'
+import { useSearchParams } from 'react-router-dom'
 
 const NewMonument = () => {
-  const { clickedSpot } = useAppStore()
-  const [markerPosition, setMarkerPosition] = useState({ ...clickedSpot });
-  const [isDragging, setIsDragging] = useState(false);
+  const [searchParams, setSearchParams] = useSearchParams()
+  const lat = searchParams.get('lat')
+  const lng = searchParams.get('lng')
+  const [address, setAddress] = useState({})
+  // const [isDragging, setIsDragging] = useState(false)
+  const [loading, setLoading] = useState(false)
+
+  useEffect(() => {
+    const fetchAddress = async () => {
+      try {
+        setLoading(true)
+        const result = await axios.post('http://localhost:5001/api/monuments/get-address', {
+          longitude: lng, latitude: lat
+        })
+        setAddress(result.data.data.address || 'Unknown address')
+      } catch (error) {
+        console.error('Error fetching address:', error)
+        setAddress('Error fetching address')
+      } finally {
+        setLoading(false)
+      }
+    }
+
+    fetchAddress()
+  }, [lat, lng]) // Runs when markerPosition changes
+  
+  // TODO handle submit form
   const onSubmitForm = async (e) => {
     e.preventDefault()
-    const { lat: latitude, lng: longitude }= markerPosition
-    console.log(markerPosition)
+
     const formData = new FormData(e.target)
     const name = formData.get('name')
-
+    const description = formData.get('description')
     const result = await axios.post('http://localhost:5001/api/monuments/', {
-      name, latitude, longitude
+      name, latitude: lat, longitude: lng, description
     })
     console.log(result)
   }
 
   const handleMarkerDragStart = useCallback(() => {
-    setIsDragging(true);
+    // setIsDragging(true)
   }, [])
 
   const handleMarkerDragEnd = useCallback((event) => {
-    setIsDragging(false);
-    setMarkerPosition({
-      lng: event.lngLat.lng,
-      lat: event.lngLat.lat,
-    });
-  }, [])
+    const { lng, lat } = event.lngLat
+    setSearchParams({ lat, lng }) // Updates URL params, triggering useEffect
+  }, [setSearchParams])
 
   return (
     <div style={{display:'flex'}}>
       <form style={{width:'1200px'}} onSubmit={onSubmitForm}>
         <div>
           <label>name</label>
-          <input name="name"></input>
+          <input name="name" required></input>
         </div>
         <div>
           <label>description</label>
-          <input name="description"></input>
+          <input name="description" required></input>
+        </div>
+        <div>
+          <label>road</label>
+          <input name="road" disabled value={address?.road + ' '+ address?.street_number}></input>
+        </div>
+        <div>
+          <label>city</label>
+          <input name="road" disabled value={address?.city}></input>
+        </div>
+        <div>
+          <label>zip code</label>
+          <input name="road" disabled value={address?.postcode}></input>
         </div>
         <button>Create</button>
       </form>
@@ -54,9 +84,10 @@ const NewMonument = () => {
         <GeolocateControl position="top-left" />
         <NavigationControl position="top-left" />
         <Marker
-          longitude={markerPosition.lng}
-          latitude={markerPosition.lat}
+          longitude={lng}
+          latitude={lat}
           anchor="bottom"
+          draggable
           onDragStart={handleMarkerDragStart}
           onDragEnd={handleMarkerDragEnd}
         >
