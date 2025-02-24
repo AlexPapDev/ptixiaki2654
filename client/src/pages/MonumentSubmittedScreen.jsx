dcf6528aa84fe976afbea02bdf0cf2395deb2676
gaspalex2011@gmail.com <gaspalex2011@gmail.com>
2025-02-24 20:53:50 +0200

    MonumentSubmittedScreen

---
+++ client/src/pages/MonumentSubmittedScreen.jsx
@@ -0,0 +1,12 @@
+import { useEffect } from 'react'
+import { Link } from 'react-router-dom'
+
+
+const MonumentSubmitted = () => {
+  
+  return (<>
+    <div>Monument Succesfully Submitted</div>
+  </>)
+}
+
+export default MonumentSubmitted
