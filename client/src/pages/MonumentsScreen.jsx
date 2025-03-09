54b3b37e52e134db684f81224e55c4153174ba96
alexpapadopoulos <apapadopoulos@deloitte.gr>
2025-03-09 21:26:49 +0200

    monuments screen cleanup

--- client/src/pages/MonumentsScreen.jsx
+++ client/src/pages/MonumentsScreen.jsx
@@ -1,12 +1,9 @@
-import { useEffect, useState } from 'react'
+import { useState, useEffect } from 'react'
 import Map from '../components/Map'
 import axios from 'axios'
 import { useSearchParams } from 'react-router-dom'
 import useAppStore from '../utils/AppStore'
 import MonumentCard from '../components/MonumentCard'
-// import { search } from '../../../server/routes/monumentRoute'
-
-// import './Navbar.css' // External CSS for navbar
 
 const Monuments = () => {
   const { searchTerm, mapBounds } = useAppStore()
@@ -14,45 +11,40 @@ const Monuments = () => {
   const passedTerm = searchParams.get('q') || searchTerm
 
   const [monuments, setMonuments] = useState([])
-  // const [mapHasLoaded, setMapHasLoaded] = useState(false)
   const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001'
-  const fetchData = async ({passedTerm, mapBounds}) => {
-    if (!mapBounds) return
 
+  const fetchData = async () => {
+    if (!mapBounds) return
     try {
       const result = await axios.get(`${API_BASE_URL}/api/monuments/`, {
         params: { 
-          query: passedTerm, mapBounds: {
+          query: passedTerm, 
+          mapBounds: {
             ne: { ...mapBounds._ne },
             sw: { ...mapBounds._sw },
           }
         }
       })
-      const { monuments } = result.data.data
-      setMonuments(monuments)
-      // setMapHasLoaded(true)
+      setMonuments(result.data.data.monuments)
     } catch (error) {
       console.error('Error fetching monuments:', error);
     }
   }
 
   useEffect(() => {
-    fetchData({passedTerm, mapBounds})
-  }, [])
-
-  useEffect(() => {
-    fetchData({passedTerm, mapBounds})
+    fetchData()
   }, [passedTerm, mapBounds])
 
   return (
     <div style={{display:'flex'}}>
       <section className="content_section grid m-t-1">
-        {monuments?.map((monument, i, arr) => <MonumentCard monument={monument} className="cell" key={'monument-card-' + i}></MonumentCard>)}
+        {monuments?.map((monument, i) => (
+          <MonumentCard monument={monument} className="cell" key={'monument-card-' + i} />
+        ))}
       </section>
       <section className="map_section">
-        {<Map data={monuments} fetchData={fetchData}></Map>}
+        <Map data={monuments} fetchData={fetchData} />
       </section>
-      
     </div>
   )
 }
