393591d601473d0c7274d95f02565ff6e88caa40
alexpapadopoulos <apapadopoulos@deloitte.gr>
2025-04-07 00:53:55 +0300

    frontend: search input without text fix

--- client/src/components/SearchInput.jsx
+++ client/src/components/SearchInput.jsx
@@ -9,7 +9,10 @@ const NavSearchBar = () => {
   const theme = useMantineTheme()
   const [inputTerm, setInputTerm] = useState(searchTerm || '')
   const navigate = useNavigate()
-  const onClickButton = () => {
+  const onClickButton = (e) => {
+    // user clicked search without inserting text
+    if (!inputTerm && !e.target.value) return
+
     // TODO: do we really need this? maybe remove
     setSearchTerm(inputTerm)
     navigate(`/monuments?q=${inputTerm}`)
