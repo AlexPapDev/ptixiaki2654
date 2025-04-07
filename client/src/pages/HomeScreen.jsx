25c4d148ffc39d2042fc966bc45f0a01ba4a57bf
alexpapadopoulos <apapadopoulos@deloitte.gr>
2025-04-07 19:29:19 +0300

    frontend: new home screen

--- client/src/pages/HomeScreen.jsx
+++ client/src/pages/HomeScreen.jsx
@@ -1,21 +1,91 @@
-import { useEffect } from 'react'
-import { Link } from 'react-router-dom'
-import axios from 'axios'
-
-
+import { useEffect, useState } from 'react'
+import { useNavigate } from 'react-router-dom'
+import TabButton from '../components/TabButton'
+import { TextInput, Button, Text, Container, Title, Group } from '@mantine/core'
+import { Search, Newspaper, Library, Landmark } from 'lucide-react'
+import useAppStore from '../utils/AppStore'
 const Home = () => {
-  useEffect(() => {
-    const fetchData = async () => {
-      const monuments = [] //await axios.get(`http://localhost:5001/api/monuments`)
-      console.log(monuments)
+  const [activeTab, setActiveTab] = useState('monuments')
+  const [inputTerm, setInputTerm] = useState('')
+  const navigate = useNavigate()
+  const { setSearchTerm, searchTerm } = useAppStore()
+  const placeholder = `Search ${activeTab}`
+
+  const handleTabChange = (tab: string) => {
+    setActiveTab(tab)
+  }
+
+  const handleSearch = () => {
+    if (!inputTerm.trim()) return
+
+    const routeMap = {
+      monuments: '/monuments',
+      lists: '/lists',
+      articles: '/articles',
     }
-    fetchData()
-  }, [])
-  
-  return (<div style={{padding: '32px', textAlign: 'align-left'}}>
-    <h2 className="align-left">Discover Greece's rich history</h2>
-    <h3 className="align-left" style={{paddingTop: '20px'}}>Recent Activity:</h3>
-  </div>)
+
+    const path = routeMap[activeTab]
+    if (path) {
+      const trimmedTerm = inputTerm.trim()
+      setSearchTerm(trimmedTerm)
+      navigate(`${path}?q=${encodeURIComponent(inputTerm.trim())}`)
+    }
+  }
+
+  return (
+    <Container align="center" style={{ alignItems: 'center', justifyContent: 'center', padding: '32px', textAlign: 'center' }}>
+      <Title mb="sm" order={1}>Search</Title>
+      <Group gap='lg' align='center' justify="center">
+        <TabButton
+          isActive={activeTab === 'monuments'}
+          Icon={Landmark}
+          onClick={() => handleTabChange('monuments')}
+        >
+          Monuments
+        </TabButton>
+        <TabButton
+          isActive={activeTab === 'lists'}
+          Icon={Library}
+          onClick={() => handleTabChange('lists')}
+        >
+          Lists
+        </TabButton>
+        <TabButton
+          isActive={activeTab === 'articles'}
+          Icon={Newspaper}
+          onClick={() => handleTabChange('articles')}
+        >
+          Articles
+        </TabButton>
+      </Group>
+      <Group justify="center">
+        <TextInput
+          mt="md"
+          radius="100px"
+          placeholder={placeholder}
+          rightSectionWidth={116}
+          onChange={(e) => setInputTerm(e.target.value)}
+          value={inputTerm}
+          style={{ width: '840px' }}
+          size="xl"
+          leftSection={
+            <Search size={24} />
+          }
+          rightSection={
+            <Button
+              size="lg"
+              radius="100px"
+              color="primary"
+              onClick={handleSearch}
+              disabled={!inputTerm.trim()}
+            >
+              <Text fw={600}>Search</Text>
+            </Button>
+          }
+        />
+      </Group>
+    </Container>
+  )
 }
 
 export default Home
