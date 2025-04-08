4663b47cc174a9bd1adbd8955a50956935920bf2
alexpapadopoulos <apapadopoulos@deloitte.gr>
2025-04-08 19:09:34 +0300

    frontend: helper for cloudinary url

--- client/src/utils/helpers.js
+++ client/src/utils/helpers.js
@@ -1,6 +1,11 @@
 const getCloudinaryUrl = (publicId, options = {}) => {
   if (!publicId) return ''
 
+  if (Array.isArray(publicId)) return publicId.map(pId => _getSingleUrl(pId, options))
+  else return _getSingleUrl(publicId, options)
+}
+
+const _getSingleUrl = (publicId, options) => {
   const baseUrl = 'https://res.cloudinary.com/djuuwduyx/image/upload/'
   const { width = 300, crop = 'fill', quality = 'auto', gravity = 'auto' } = options
   const transformation = `w_${width},c_${crop},q_${quality},g_${gravity}`
