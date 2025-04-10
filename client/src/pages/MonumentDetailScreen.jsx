3c7d3e19beaa00c24191253aa568bc7058616eaf
alexpapadopoulos <apapadopoulos@deloitte.gr>
2025-04-10 23:43:39 +0300

    frontend: monumentdetail refactor

--- client/src/pages/MonumentDetailScreen.jsx
+++ client/src/pages/MonumentDetailScreen.jsx
@@ -1,70 +1,78 @@
-import { useState, useEffect } from 'react'
-import { useParams } from 'react-router-dom'
-import axios from 'axios'
-import { Container, Title, Group, Button, Box, Text, Stack, Divider, Grid } from '@mantine/core'
+import { useParams, useNavigate } from 'react-router-dom'
+import useAuthStore from '../utils/AuthStore'
+import {
+  Container,
+  Title,
+  Group,
+  Button,
+  Box,
+  Text,
+  Divider,
+  Grid,
+  Center,
+  Loader,
+} from '@mantine/core'
+import useMonumentDetail from '../hooks/useMonumentDetail'
+import useDeleteMonument from '../hooks/useDeleteMonument'
 import MonumentDetailGrid from '../components/MonumentDetailGrid'
 import MonumentDetailTextInfo from '../components/MonumentDetailTextInfo'
 import MonumentDetailMap from '../components/MonumentDetailMap'
 import WorkingHours from '../components/WorkingHours'
+
+import { toast } from 'react-toastify'
 const MonumentDetail = () => {
   const { monumentId } = useParams()
-  const [monument, setMonument] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState(null)
+  const navigate = useNavigate()
+  const { isLoggedIn } = useAuthStore()
 
-  const API_BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:5001'
-  // const url = getCloudinaryUrl(monument?.images[0])
-  useEffect(() => {
-    const fetchMonument = async () => {
-      try {
-        setLoading(true)
-        const result = await axios.get(`${API_BASE_URL}/api/monuments/${monumentId}`)
-        const { monument } = result.data.data
-        const { name, description, latitude, longitude, categories = [], images = [], address, address: { city, road }} = monument
-        setMonument({
-          latitude, longitude, name, description, city, road, categories, images, address, isPublic: true
-        })
-        
-      } catch (err) {
-        setError('Failed to load monument details')
-      } finally {
-        setLoading(false)
-      }
-    }
+  const { monument, loading, error } = useMonumentDetail(monumentId)
+  const { handleDeleteClick } = useDeleteMonument(monumentId, () => {
+    toast.success('Record deleted successfully!', { position: 'top-right' })
+    navigate('/monuments', { replace: true })
+  })
 
-    if (monumentId) {
-      fetchMonument()
-    }
-  // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [monumentId])
-
-  if (loading) return <div>Loading...</div>
-  if (error) return <div>{error}</div>
+  if (loading || error) {
+    return (
+      <Center h="100%">
+        {loading ? <Loader /> : <Text color="red">{error}</Text>}
+      </Center>
+    )
+  }
 
   return (
     <Container>
       <Group pt="lg">
         <Title order={2}>{monument?.name}</Title>
-        <Button color="teal">Edit</Button>
-        <Button color="coral">Delete</Button>
+        {isLoggedIn() && <>
+          <Button color="teal">Edit</Button>
+          <Button color="red" onClick={handleDeleteClick}>Delete</Button>
+          </>}
       </Group>
-      
-      <div mt="lg">
+
+      <Box mt="lg">
         <MonumentDetailGrid images={monument?.images} />
-      </div>
+      </Box>
+
       <Grid>
         <Grid.Col span={7}>
           <MonumentDetailTextInfo monument={monument} />
-          <Divider pb="md"/>
-          <WorkingHours hoursPerDay={[]} isPublic={monument.isPublic}/>
+          <Divider pb="md" />
+          {monument.workingHours?.length > 0 && (
+            <WorkingHours
+              hoursPerDay={monument.workingHours}
+              isPublic={monument.isPublic}
+            />
+          )}
         </Grid.Col>
         <Grid.Col pl="md" span={5} pt="xl">
-          <Box style={{height:'100%', width: '100%'}}>
-            <MonumentDetailMap lat={monument?.latitude} lng={monument?.longitude}></MonumentDetailMap>
+          <Box style={{ height: '100%', width: '100%' }}>
+            <MonumentDetailMap
+              lat={monument?.latitude}
+              lng={monument?.longitude}
+            />
           </Box>
         </Grid.Col>
       </Grid>
-      
     </Container>
   )
 }
