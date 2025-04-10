66147b193765dc3f75c267c91eb1094db39dabb3
alexpapadopoulos <apapadopoulos@deloitte.gr>
2025-04-11 00:05:31 +0300

    frontend: small visual navbar updates

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,54 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Image, Group, Container, Anchor, Text } from '@mantine/core'
+import { Image, Group, Container, Text, Anchor, Box } from '@mantine/core'
 import SearchInput from './SearchInput'
 import ProfileNav from './ProfileNav'
+
+const links = [
+  { link: '/monuments', label: 'Monuments' },
+  { link: '/lists', label: 'Lists' },
+  { link: '/articles', label: 'Articles' },
+]
 
 const Navbar = () => {
   return (
     <Container fluid px={32} pt={16} pb="md">
-      <Group justify="space-between">
-        <Group gap="xs">
-          <Image height={30} width={30} src="/ancient-greece.png"></Image>
+      <Group justify="space-between" style={{ position: 'relative', minHeight: '42px' }}>
+        <Group gap="xs" visibleFrom="md">
+          <Image height={30} width={30} src="/ancient-greece.png" />
           <Link to="/" style={{ textDecoration: 'none' }}>
-            <Text color='primary' size="md" fw={600}>Monuma</Text>
+            <Text color="primary" size="md" fw={600}>
+              Monuma
+            </Text>
           </Link>
         </Group>
-        
-        <Group gap="md" align="center">
-          <SearchInput />
-        </Group>
 
-        <ProfileNav />
+        {/* This is the part that will be absolutely positioned */}
+        <Box style={{
+          position: 'absolute',
+          left: '50%',
+          transform: 'translateX(-50%)',
+        }}>
+          <SearchInput />
+        </Box>
+
+        <Group ml={50} gap="md" visibleFrom="lg">
+          {links.map((link) => (
+            <Box
+              key={link.label}
+              sx={{
+                backgroundColor: 'red',
+                padding: '8px',
+                borderRadius: '4px',
+              }}
+            >
+              <Anchor component={Link} to={link.link}>
+                <Text>{link.label}</Text>
+              </Anchor>
+            </Box>
+          ))}
+          <ProfileNav />
+        </Group>
       </Group>
     </Container>
   )
