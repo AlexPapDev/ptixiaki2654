f09638082c92c07d4f8a73aeeaa363e9f3194cc8
alexpapadopoulos <apapadopoulos@deloitte.gr>
2025-06-20 14:19:33 +0300

    frontend: monument detail edit button rename for generic

--- client/src/components/EditButton.jsx
+++ client/src/components/EditButton.jsx
@@ -1,10 +1,10 @@
 import { ActionIcon} from '@mantine/core'
 import { Pencil } from 'lucide-react'
-const MonumentDetailEditButton = ({onEdit}) => {
+const EditButton = ({onEdit}) => {
   return (
     <ActionIcon variant="outline" aria-label="Settings" >
       <Pencil size={18} onClick={onEdit}/>
     </ActionIcon>
   )
 }
-export default MonumentDetailEditButton
+export default EditButton
