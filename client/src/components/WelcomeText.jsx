b4625a9ceb1fe6e5bec820eb8ae4ddcb707934a4
gaspalex2011@gmail.com <gaspalex2011@gmail.com>
2025-06-25 20:11:25 +0300

    WelcomeText fix

--- client/src/components/WelcomeText.jsx
+++ client/src/components/WelcomeText.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
-import { Title, Text, Container, Center } from '@mantine/core'
+import { Title, Text } from '@mantine/core'
 
-const NewMonumentMap = ({handleMarkerDragEnd, lat, lng}) => {
+const WelcomeText = () => {
   return (<>
     <Title>Καλώς ήρθατε στην Ιστορία της Θεσσαλονίκης!</Title>
     <Text>Εξερευνήστε τα μνημεία της πόλης και ανακαλύψτε τα μυστικά τους. Αναζητήστε, δημιουργήστε τις δικές σας λίστες, επεξεργαστείτε πληροφορίες και αλληλεπιδράστε με την πλούσια κληρονομιά της Θεσσαλονίκης. Ξεκινήστε το ταξίδι σας στην ιστορία τώρα!</Text>
   </>)
 }
-export default NewMonumentMap
+export default WelcomeText
